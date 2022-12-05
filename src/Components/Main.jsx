3e11fc9f7546597c3ef83ba7776d8392d877c66c
jaikrishnaverma-cedcoss <jaikrishnaverma@cedcoss.com>
2022-12-05 12:04:25 +0530

    optimize

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,19 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { data } from '../data'
 import SelectInput from './SelectInput'
 const Main = () => {
-    const [state, setState] = useState({ optionData: [], tableData: [] })
-
-    // initial data 
-    useEffect(() => {
+    
+    const [state, setState] = useState((JSON.parse(localStorage.getItem("state"))!==null)?JSON.parse(localStorage.getItem("state")):{ optionData: [], tableData: [],showTable:[],selected:[]})
+ 
+    // initial Table data 
+    if(state.optionData.length==0){
         const category = []
         data.map(x => {
             if (category.indexOf(x.primary_category) == -1)
                 category.push(x.primary_category)
         })
         state.optionData.push(category)
-        setState({ ...state })
-    }, [])
+        setState({ ...state })   
+    }
 
     // Make Search for Table data
     const searchFor = (value, level, previousArr = data) => {
@@ -22,13 +23,12 @@ const Main = () => {
     }
 
     // Make unique Options
-    const secondSearch = (CategoryArr, level) => {
+    const optionsGenerator = (CategoryArr, level) => {
         (level == 0) ? level = 'category_1' : level = 'category_' + (level + 1)
         let unique = []
         CategoryArr.map(x => {
             if (unique.indexOf(x[level]) == -1) {
-                unique.push(x[level])
-            }
+                unique.push(x[level]) }
         })
         return unique
     }
@@ -37,32 +37,37 @@ const Main = () => {
     const onSelected = (value, level) => {
         if(value=="-1")
         return ''
-        let tmpLevel
-        (level == 0) ? tmpLevel = "primary_category" : tmpLevel = 'category_' + level
+        let tmpLevel=(level == 0) ? "primary_category" : 'category_' + level
         state.tableData = searchFor(value, tmpLevel)
         if (state.optionData.length > level) {
             state.optionData.splice(level + 1)
-            state.optionData[level + 1] = secondSearch(state.tableData, level)
+            state.selected.splice(level)
+            state.selected.push(value)
+            state.optionData[level + 1] = optionsGenerator(state.tableData, level)
+        }else{          
+         state.optionData.push(optionsGenerator(state.tableData, level))
+         state.selected.push(value) 
         }
-        else
-            state.optionData.push(secondSearch(state.tableData, level))
-        setState({ ...state })
+         setState({ ...state })
     }
-    
+
     return (
         <>
-            <div className="main">
-                <div className="container">
+            <div className="main ">
+                <div className="container brd">
                     <h1>Products</h1>
                     {
-                        state.optionData && state.optionData.map((x, i) => <SelectInput key={x} onSelected={onSelected} options={x} level={i} />)
+                        state.optionData && state.optionData.map((x, i) => <SelectInput key={x} onSelected={onSelected} options={x} level={i} value={state.selected}/>)
                     }
-                    <button>
-                        Save
+                    <button id="btn1" onClick={()=>{localStorage.setItem("state",JSON.stringify(state));setState({...state,showTable:[...state.tableData]});console.log("state",state)}}>
+                        Save to Local Storage
+                    </button>
+                    <button onClick={()=>localStorage.setItem("state",JSON.stringify({ optionData: [], tableData: [],showTable:[],selected:[]}))}>
+                        Clear Local Storage
                     </button>
                     <table>
                         <thead>
-                            <tr>
+                            <tr>    
                                 <th>Product Category</th>
                                 <th>Category 1</th>
                                 <th>Category 2</th>
@@ -71,7 +76,7 @@ const Main = () => {
                         </thead>
                         <tbody>
                             {
-                                state.tableData.map(x => <tr key={x.asin+x.img_source} ><td>{x.primary_category}</td><td>{x.category_1}</td>
+                                state.showTable.map(x => <tr key={x.asin+x.img_source} ><td>{x.primary_category}</td><td>{x.category_1}</td>
                                     <td>{x.category_2}</td><td>{x.category_3}</td></tr>)
                             }
                         </tbody>
--- src/Components/SelectInput.jsx
+++ src/Components/SelectInput.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 
-const SelectInput = ({onSelected , options , level}) => {
+const SelectInput = ({onSelected , options , level , value}) => {
+  
     if(options.length==0||options[0]==''||options==undefined||options[0]==undefined)
     return <p>NO DATA FOUND</p>
-    return (
-        <>
+ 
+    return (<>
             <select name="" id="" onChange={(e)=>onSelected(e.target.value,level)}>
             <option key={"sda"} value="-1">--SELECT</option>
                 {
-                    options&&options.map(category => <option key={level+category} value={category}>{category}</option>)
+                    options&&options.map((category,i) => {
+                        return  (value[level]==category) 
+                        ? <option  key={level+i+category} value={category} selected>{category}</option>
+                        : <option  key={level+i+category} value={category}>{category}</option>})
                 }
             </select>
-        </>
-    )
+        </>)
 }
 
 export default SelectInput
